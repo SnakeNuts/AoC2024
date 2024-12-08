eed565d1a88fecb79e03bdfa5fb1c75289d255a1
Alex Crouzen <alexander@crouzen.com>
2024-12-08 20:03:51 +0000

    Day2 Complete

--- day_02.py
+++ day_02.py
@@ -1,21 +1,38 @@
+from itertools import combinations
 
 def run_1(input_file_name):
-  run(input_file_name)
-
-def run_2(input_file_name):
-  run(input_file_name, dampner = True)
-
-def run(input_file_name, dampner = False):
   safe_count = 0
   with open(input_file_name) as input:
     reports = input.readlines()
     for report in reports:
-      safe_count += is_report_safe(report, dampner)
+      levels = report.split()
+      error_count = errors_in_report(levels)
+      safe_count += (error_count < 1)
   print(safe_count)
 
-def is_report_safe(report, dampner) -> int:
+def run_2(input_file_name):
+  safe_count = 0
+  with open(input_file_name) as input:
+    reports = input.readlines()
+    for report in reports:
+      levels = report.split()
+      error_count = errors_in_report(levels)
+      if error_count == 0:
+        safe_count += 1
+      else:
+        for new_report in combinations(levels,len(levels)-1):
+          if errors_in_report(new_report) == 0:
+            safe_count += 1
+            break
+        
+  print(safe_count)
+
+
+
+
+
+def errors_in_report(levels) -> int:
   differences = list()
-  levels = report.split()
   errors = 0
   for index in range(1, len(levels)):
     difference = int(levels[index]) - int(levels[index - 1])
@@ -25,13 +42,7 @@ def is_report_safe(report, dampner) -> int:
   # check if all differences are either up or down
   if not all_same_sign(differences):
     errors += 1
-  if errors == 0:
-    return 1
-  else:
-    if dampner and errors == 1:
-      return 1
-    else:
-      return 0
+  return errors
 
 def all_same_sign(numbers):
   sign = numbers[0] >= 0
@@ -39,4 +50,5 @@ def all_same_sign(numbers):
 
 
 if __name__ == "__main__":
+  run_1("day_02_input.txt")
   run_2("day_02_input.txt")
