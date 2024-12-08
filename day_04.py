6e519e8774a5035175965a03dcad7f328e857538
Alex Crouzen <alexander@crouzen.com>
2024-12-08 23:08:12 +0000

    Day4 progress

---
+++ day_04.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def run_1(filename):
+    with open(filename) as input:
+        lines = input.readlines()
+        
+
+if __name__ == "__main__":
+    run_1("day_04_test_input.txt")
